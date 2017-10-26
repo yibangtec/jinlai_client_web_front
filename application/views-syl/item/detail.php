94b9ce9cfb944c65aa4989d0c389266e55a60b0f
syl <10716710@qq.com>
2017-10-26 17:48:44 +0800

    Signed-off-by: syl <10716710@qq.com>

--- application/views-syl/item/detail.php
+++ application/views-syl/item/detail.php
@@ -34,9 +34,11 @@
 		right: .2rem;
 	}
 	.detailshare img{
-		display: block;
-		margin-top: .08rem;
-		margin-left: .3rem;
+		    display: block;
+    margin-top: .08rem;
+    margin-left: .3rem;
+    width: .38rem;
+    height: .39rem;
 	}
 	#service-promise ul{
 		width: 5.76rem;
@@ -44,6 +46,17 @@
 		justify-content: space-between;
 	}
 	#service-promise li {font-size:.24rem;color:#898989;/*background:url('/media/item/detail/quan@3x.png') no-repeat left center;background-size:24px 24px;*/}
+	#service-promise li span{
+		    width: .24rem;
+    height: .24rem;
+    float: left;
+    margin-right: .1rem;
+	}
+	#service-promise li span img{
+		display: block;
+		width: 100%;
+		height: 100%;
+	}
 
 	#prices {font-size:.24rem}
 		#prices strong {color:#ff3649;font-size:44px;font-weight:600;}
@@ -57,7 +70,7 @@
 			#skus figure {width:28px;height:28px;}
 			#skus h3 {font-size:12px;max-width:97px;overflow: hidden;text-overflow: ellipsis;white-space: nowrap;text-indent: 0;padding-left:1px;}
 
-	#general-seperater {font-size:24px;background-color:transparent;text-align:center;margin:50px 0;}
+	#general-seperater {font-size:.24rem;background-color:transparent;text-align:center;margin:.5rem 0;}
 
 	#description p, #description img {line-height:1;}
 	.tipprice{
@@ -93,6 +106,14 @@
 		margin: 0 .2rem;
 	}
 	.topcardwrap .get{
+		box-shadow: 0px 0px 0.15rem #ffc7bc;
+    background: linear-gradient(right , #ff7a42 6% , #ff415a 100%) !important;
+    background: -o-linear-gradient(right , #ff7a42 6% , #ff415a 100%) !important;
+    background: -ms-linear-gradient(right , #ff7a42 6% , #ff415a 100%) !important;
+    background: -moz-linear-gradient(right , #ff7a42 6% , #ff415a 100%) !important;
+    background: -webkit-linear-gradient(right , #ff7a42 6% , #ff415a 100%) !important;
+    color: #fff;
+    font-size: .26rem;
 		    width: .76rem;
     height: .4rem;
     font-size: .16rem;
@@ -141,6 +162,10 @@
 		display: block;
 		float: left;
 		margin-left: .1rem;
+		font-size: .26rem;
+		color: #3e3a39;
+	}
+	.topcardwrap h2{
 		font-size: .26rem;
 		color: #3e3a39;
 	}
@@ -242,7 +267,7 @@ wx.ready(function(){
 	<section id=item-brief>
 		<!--分享-->
 		<div class="detailshare">
-			<img src="<?php echo CDN_URL ?>media/item/detail/fenxiang@3x.png" />
+			<img src="<?php echo CDN_URL ?>media/item/detail/fengxiang@3x.png" />
 		</div>
 		<h2 id=item-name><?php echo $item['name'] ?></h2>
 
@@ -282,11 +307,23 @@ wx.ready(function(){
 	<section id=service-promise>
 		<ul>
 			<li>
-				<span></span>
+				<span>
+					<img src="<?php echo CDN_URL ?>media/item/detail/quan@3x.png"/>
+				</span>
 				上门处理投诉
 			</li>
-			<li>满58元包邮</li>
-			<li>3小时送达</li>
+			<li>
+				<span>
+					<img src="<?php echo CDN_URL ?>media/item/detail/quan@3x.png"/>
+				</span>
+				满58元包邮
+			</li>
+			<li>
+				<span>
+					<img src="<?php echo CDN_URL ?>media/item/detail/quan@3x.png"/>
+				</span>
+				3小时送达
+			</li>
 		</ul>
 	</section>
 
@@ -321,59 +358,224 @@ wx.ready(function(){
 		</div>
 		
 	</div>
-		<div id=skus class="wid710 auto border20 bgfff skus clearfix mt20">
+		<div class="wid710 auto border20 bgfff skus clearfix mt20">
 		<!--购物券区域-->
 		<div class="topcardwrap">
-			<h1 class="clearfix">
-				<i>全进来实物商品通用</i>
-			</h1>
-			<h1 class="clearfix">
-				<i>领取优惠券</i>
-			</h1>
-			
+			<h2>产品参数</h2>
 		</div>
 		<hr>
 		<div class="topcardwrap">
-			<h1 class="clearfix">
-				<span>进来购物券</span>
-				<span>全进来实物商品通用</span>
-			</h1>
-			<h1 class="clearfix">
-				<span>店铺优惠券</span>
-				<span>领取优惠券</span>
-			</h1>
+			<h2>配送范围:市南区、市北区大部、崂山区大部。</h2>
 		</div>
 		
 	</div>
-	<section id=biz-info class=row>
-		<a title="<?php echo $biz['name'] ?>" href="<?php echo base_url('biz/detail?id='.$item['biz_id']) ?>">
-			<?php echo $biz['name'] ?>
-		</a>
-	</section>
-
-	<p id=general-seperater>继续拖动，查看图文详情</p>
-
-	<div id=description class=row>
-		<?php if ( !empty($item['description']) ): ?>
-		<div id=description-content>
-			<?php echo $item['description'] ?>
+	<!--商品评价区域-->
+	<div class="productevaluation mt20 auto wid710 border20 clearfix">
+		<div class="topcardwrap">
+			<h2>商品评价  ( 1967 )  </h2>
 		</div>
-		<?php endif ?>
-
-		<div id=common-content>
-			<h3>平台统一内容</h3>
+		
+		<hr />
+		<div class="topcardwrap clearfix">
+			<div class="evaluationinfo clearfix">
+				<div class="pic fl">
+					<img src="<?php echo CDN_URL ?>media/item/detail/ximei@3x.png"/>
+				</div>
+				<span class="fl block">150****4.32</span>
+				<time class="fr">2017-03-22</time>
+			</div>
+		</div>
+		<!--商品评价内容区域-->
+		<div class="evaluationcontent">
+			<p>商品一如既往的好,实物和照片一样没有色差,口感非常好,以后一如既往的关注意帮管家平台的,希望平台以后多搞一些这样的</p>
+		</div>
+		<div class="evaluationbtn auto">
+			查看全部评价
 		</div>
 	</div>
+	
+	<!--店铺信息区域-->
+	<div class="shopInfo mt20 auto border20 clearfix">
+		<div class="headerinfo clearfix">
+			<div class="pic fl">
+				<img src="<?php echo CDN_URL ?>media/item/detail/ximei@3x.png" />
+			</div>
+			<div class="headertext fl">
+				<h1>进来自营</h1>
+				<h2>入驻进来平台</h2>
+			</div>
+		</div>
+		<div class="shopinfolist clearfix">
+			<ul class="clearfix">
+				<li>
+					<p>20</p>
+					<p>全部宝贝</p>
+				</li>
+				<li>
+					<p>11.1万</p>
+					<p>关注人数</p>
+				</li>
+				<li>
+					<span>
+						宝贝描述
+						<i>4.8高</i>	
+					</span>
+					<span>
+						卖家服务
+						<i>4.8高</i>	
+					</span>
+					<span>
+						物流服务
+						<i>4.8高</i>	
+					</span>
+				</li>
+			</ul>
+		</div>
+		<!--查看分类-->
+		<div class="shopmore">
+			<a href="##">
+				查看分类
+			</a>
+			<a href="##">
+				进店逛逛
+			</a>
+		</div>
+	</div>
+		<!--热门活动-->
+		<div class="hotactivetitle wid710 auto mt20 border20">
+			<img src="<?php echo CDN_URL ?>media/item/detail/remenhuodong@3x.png" />
+		</div>
+		<div class="activecontent mt20 wid710 auto border20 clearfix">
+			<ul class="clearfix">
+				<li>
+					<img src="<?php echo CDN_URL ?>media/item/detail/ximei@3x.png"/>
+					<h1>西梅</h1>
+					<h2>400g/份</h2>
+					<h3>¥29.80</h3>
+				</li>
+				<li>
+					<img src="<?php echo CDN_URL ?>media/item/detail/tao@3x.png"/>
+					<h1>西梅</h1>
+					<h2>400g/份</h2>
+					<h3>¥29.80</h3>
+				</li>
+				<li>
+					<img src="<?php echo CDN_URL ?>media/item/detail/pingguo@3x.png"/>
+					<h1>西梅</h1>
+					<h2>400g/份</h2>
+					<h3>¥29.80</h3>
+				</li>
+				<li style="margin-right: 0;">
+					<img src="<?php echo CDN_URL ?>media/item/detail/juzi@3x.png"/>
+					<h1>西梅</h1>
+					<h2>400g/份</h2>
+					<h3>¥29.80</h3>
+				</li>
+			</ul>
+			
+		</div>
+	<p id=general-seperater>继续拖动，查看图文详情</p>
+	<div class="bigadd border20 wid710 auto">
+		<img src="<?php echo CDN_URL ?>media/item/detail/tu@3x.png" />
+	</div>
+	<div class="particularstag wid710 auto">
+		<img src="<?php echo CDN_URL ?>media/item/detail/shangpinliangdian@3x.png" class="pictitle"/>
+		<img src="<?php echo CDN_URL ?>media/item/detail/tu1@3x.png" class="piccontent" />
+		<p>
+			健脑、抗氧化、预防糖尿病、增强食欲、促进代谢、美容养颜、显著改善贫血等。适合老人、
+			小孩及孕妇食用。
+		</p>
+	</div>
+	<div class="particularstag wid710 auto">
+		<img src="<?php echo CDN_URL ?>media/item/detail/ruhetiaoxuan@3x.png" class="pictitle"/>
+		<img src="<?php echo CDN_URL ?>media/item/detail/tu1@3x.png" class="piccontent" />
+		<p>
+			血橙上一头上的圆形博文就是所谓的“脐”，血橙的脐越小越甜。如果表皮出现白色的霉点，则血橙的内部开始霉烂。
+		</p>
+	</div>
+	<div class="particularstag wid710 auto">
+		<img src="<?php echo CDN_URL ?>media/item/detail/shiyongfangshi@3x.png" class="pictitle"/>
+		<img src="<?php echo CDN_URL ?>media/item/detail/tu1@3x.png" class="piccontent" />
+		<p>
+			美国血橙适宜鲜美血橙酸奶杯
+			1、将血橙去皮切薄片（厚度约3mm）、酸奶备用。
+			2、用筷子夹取橙片，贴在玻璃杯内壁。
+			3、将备好的酸奶倒入杯中即可。
+		</p>
+	</div>
+	<div class="particularstag wid710 auto">
+		<img src="<?php echo CDN_URL ?>media/item/detail/jinlaiyoushi@3x.png" class="pictitle"/>
+		<!--特色介绍-->
+		<div class="featurescard clearfix mt10">
+			<img src="<?php echo CDN_URL ?>media/item/detail/quanchenglenglian@3x.png" />
+			<img src="<?php echo CDN_URL ?>media/item/detail/3xiaoshisongda@3x.png" />
+			<img src="<?php echo CDN_URL ?>media/item/detail/tousuchuli@3x.png" />
+		</div>
+		<!--猜你喜欢区域-->
+		<div class="shopinfolike auto wid710 border20">
+			<img src="<?php echo CDN_URL ?>media/item/detail/cainixihuan@3x.png"/>
+		</div>
+		<div class="like wid718 auto clearfix mt10">
+					<ul>
+						<li>
+							<img src="<?php echo CDN_URL ?>media/home/cainixihuan/cnxh/210x210_0.jpg"/>
+							<h1>JUSTE UN CLOU 手镯</h1>
+							<h2>8K玫瑰金</h2>
+							<h3>¥10555.00</h3>
+						</li>
+						<li>
+							<img src="<?php echo CDN_URL ?>media/home/cainixihuan/cnxh/210x210_1.jpg"/>
+							<h1>栖霞红富士</h1>
+							<h2>18k玫瑰金,手带表</h2>
+							<h3>¥1045.00</h3>
+						</li>
+						<li class="mr0">
+							<img src="<?php echo CDN_URL ?>media/home/cainixihuan/cnxh/210x210_2.jpg"/>
+							<h1>DRIVER ONE 手表</h1>
+							<h2>8K玫瑰金</h2>
+							<h3>¥5100</h3>
+						</li>
+						<li>
+							<img src="<?php echo CDN_URL ?>media/home/cainixihuan/cnxh/210x210_3.jpg"/>
+							<h1>JUSTE UN CLOU 手镯</h1>
+							<h2>8K玫瑰金</h2>
+							<h3>¥5100</h3>
+						</li>
+						<li>
+							<img src="<?php echo CDN_URL ?>media/home/cainixihuan/cnxh/210x210_0.jpg"/>
+							<h1>JUSTE UN CLOU 手镯</h1>
+							<h2>8K玫瑰金</h2>
+							<h3>¥5100</h3>
+						</li>
+						<li class="mr0">
+							<img src="<?php echo CDN_URL ?>media/home/cainixihuan/cnxh/210x210_2.jpg"/>
+							<h1>JUSTE UN CLOU 手镯</h1>
+							<h2>8K玫瑰金</h2>
+							<h3>¥5100</h3>
+						</li>
+					</ul>
+				</div>
+	</div>
+	
 </div>
 
 <style>
-	#nav-main {padding:14px 20px 14px 40px;border-top:1px solid #ddd;border-radius:20px 20px 0 0;}
-		#nav-main .btn {width:194px;height:64px;line-height:64px;margin-top:3px;border-radius:10px;}
-		#nav-main li.vice-button {margin-right:54px;}
-			#nav-main li.vice-button a {font-size:24px;line-height:1;text-align:center;}
-		#nav-main li:nth-of-type(3) {margin-right:40px;}
+	#nav-main {background:#fff;height:.83rem;    width: 7.16rem;padding:.17rem 0;padding-left:.2rem;padding-right:.2rem;border-top:1px solid #ddd;border-radius:20px 20px 0 0;font-size: .24rem;position: fixed;bottom: 0;color: #3E3A39;}
+	#nav-main li{
+		float: left;
+	}
+		#nav-main .btn {width:1.94rem;height:.64rem;line-height:.64rem;margin-top:3px;border-radius:10px;background: #fa3752;float: left;text-align: center;float: left;color: #fff;}
+		#nav-main .btn:nth-child(2){
+			background: #ff5b00;
+		}
+		#order-create{
+			background: #ff5b00 !important;
+		}
+		#nav-main li.vice-button {margin-right:.54rem;float: left;width: .5rem;}
+			#nav-main li.vice-button a {font-size:.24rem;line-height:1;text-align:center;float: left;color: #3E3A39;width: .5rem;}
+		#nav-main li:nth-of-type(3) {margin-right:.4rem;}
 		#nav-main li:last-of-type {margin-left:10px;}
-		#nav-main img {width:36px;height:36px;margin:0 auto 10px;}
+		#nav-main img {width:.36rem;height:.36rem;margin:0 auto .1rem;}
 </style>
 
 <nav id=nav-main>
@@ -381,7 +583,7 @@ wx.ready(function(){
 		<?php // TODO 显示客服按钮前检查当前店铺客服工作状态，决定留言或即时通讯 ?>
 		<li class=vice-button>
 			<a title="客服" href="<?php echo base_url('dialog/detail?biz_id='.$item['biz_id']) ?>">
-				<img src="<?php echo base_url('/media/item/detail/kafu_icon@3x.png') ?>">
+				<img src="<?php echo CDN_URL ?>media/item/detail/kafu_icon@3x.png">
 				客服
 			</a>
 		</li>
@@ -389,7 +591,7 @@ wx.ready(function(){
 		<?php // TODO 显示店铺按钮前检查商家经营状态 ?>
 		<li class=vice-button>
 			<a title="店铺" href="<?php echo base_url('biz/detail?id='.$item['biz_id']) ?>">
-				<img src="<?php echo base_url('/media/item/detail/dianpu_icon@3x.png') ?>">
+				<img src="<?php echo CDN_URL ?>media/item/detail/dianpu_icon@3x.png">
 				店铺
 			</a>
 		</li>
@@ -397,7 +599,7 @@ wx.ready(function(){
 		<?php // TODO 显示收藏、加入购物车、立即购买按钮前检查是否可售性（是否在售、库存是否足够、每单最高限量等） ?>
 		<li class=vice-button>
 			<a class=create data-op-class=fav_item data-op-name=create data-id="<?php echo $item['item_id'] ?>" title="收藏" href="<?php echo base_url('fav_item/create?id='.$item['item_id']) ?>">
-				<img data-src-success="<?php echo base_url('/media/item/detail/shoucang-_xuanzhong@3x.png') ?>" src="<?php echo base_url('/media/item/detail/shoucang-@3x.png') ?>">
+				<img data-src-success="<?php echo base_url('/media/item/detail/shoucang-_xuanzhong@3x.png') ?>" src="<?php echo CDN_URL ?>media/item/detail/shoucang-@3x.png">
 				收藏
 			</a>
 		</li>
@@ -443,4 +645,8 @@ wx.ready(function(){
 	
 	// 商品信息
 	var item = <?php echo $item_in_json ?>
+	
+	
 </script>
+
+
