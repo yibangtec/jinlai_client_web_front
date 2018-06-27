94daba32b4bf6f256f891ae51d44d649c17ee2f8
10716710@qq.com <10716710@qq.com>
2018-06-27 11:26:49 +0800

    ''

--- application/views/cart/index.php
+++ application/views/cart/index.php
@@ -1,167 +1,36 @@
-	<style>
-        body {background:#efefef;overflow-x: hidden;}
-        .member_list {
-        height:2.6rem;
-            background-color:orange;
-        /* background:url(<?php echo CDN_URL ?>media/member/tu1@3x.png); */
-        position:relative;
-        margin-bottom:.2rem;
-	    background-size:contain;
-        overflow:hidden;}
-        .ui-loader {display:none;}
-        .icon-bottom-preview:before {
-	    content: "\e999";
-	    color: #adadad;
+<style>
+/*input.check{
+	background:url(<?php echo CDN_URL ?>media/cart/icon/icon_radio3.png) no-repeat center left;  
+	 background-size: cover;
+    position: absolute;
+    top: 50%;
+    left: 10px;
+    margin-top: -.21rem;
+    width: .42rem;
+    height: .42rem;
+ }
+ input.check:checked{
+	background:url(<?php echo CDN_URL ?>media/cart/icon/icon_radio4.png) no-repeat center left;
+	background-size:contain;
+	margin-top: -.21rem;
+    width: .42rem;
+    height: .42rem;
 	}
-    </style>
-	<!--头部title区域开始-->
-	<div class="popwindowcontent">
-		<div class="wid670 auto clearfix">
-			<input type="text" placeholder="搜索会员卡" class="fl" id="search-membercard"/>
-			<i class="icon-bottom-preview"></i>
-			<span id="popwindowcancle" class="fl">取消</span>
-		</div>
-	</div>
-	<div class="member_title">
-		<i class="icon-back fl"></i>
-		会员
-		<i class="icon-huiyuan fr"></i>
-		<i class="icon-bottom-preview" id="popwindowbtn"></i>
-	</div>
-		<div class="bgfff memberclass">
-		<div class="couponcentertitle auto bgfff">
-			<div class="swiper-containercouponcenter">
-				<div class="swiper-wrapper" id="memberTitle">
-					 <div class="swiper-slide couponcentercur">
-                        <a class="couponcenter">全部</a>
-                    </div>
-                    <!--<div class="swiper-slide">
-                        <a>4S店</a>
-                    </div>
-                    <div class="swiper-slide">
-                        <a>酒店</a>
-                    </div>
-                    <div class="swiper-slide">
-                        <a>美容</a>
-                    </div>
-                    <div class="swiper-slide">
-                        <a>培训</a>
-                    </div>
-                    <div class="swiper-slide">
-                        <a>美食美客</a>
-                    </div>
-                    <div class="swiper-slide">
-                        <a>足疗店</a>
-                    </div>
-                    <div class="swiper-slide">
-                        <a>艾灸馆</a>
-                    </div>-->
-                    <div class="swiper-scrollbar123"></div>
-                </div>
-				</div>
-		</div>
-		</div>
-	<div class="member_wraplist">
-		<div class="couponcentercontent auto mt20 clearfix">
-			<div class="swiper-containercouponcentertemplate">
-				<div class="swiper-wrapper">
-					<div class="swiper-slide">
-    <?php
-        foreach ($items as $item):
-            // 若未指定会员卡LOGO，使用商家LOGO
-            $logo_url = MEDIA_URL. (!empty($item['member_logo_url'])? 'ornament_biz/'. $item['member_logo_url']: 'biz/'. $item['url_logo']);
-          	if($logo_url == 'https://jinlaisandbox-images.b0.upaiyun.com/biz/')
-            	$logo_url = 'https://cdn-remote.517ybang.com/media/home/default.png';
-            $inline_style = '';
-            if ( !empty($item['vi_color_first']) )
-                $inline_style .= 'background-color:#'.$item['vi_color_first'].';';
-            if ( !empty($item['member_thumb_url']) )
-                $inline_style .= 'background: url('. MEDIA_URL.'ornament_biz/'.$item['member_thumb_url']. ') no-repeat center center;';
-    ?>
-    
-    <a class="entermember_wrap" href="<?php echo base_url('member_biz/detail?biz_id='.$item['biz_id']) ?>" target="_self">
-        <div class="member_list wid710 auto border20" style="<?php echo $inline_style ?>">
-        	<?php
-        		if (!empty($item['member_logo_url']) || !empty($item['url_logo'])):
-        			$background_color = $item['vi_color_second'];
-        		else:
-        			$background_color = 'efefef';
-        		endif;
-        	?>
-            <div class="memberlistpic" style="background-color:#<?php echo $background_color ?>">
-                <img src="<?php echo $logo_url ?>">
-            </div>
-            <h1><?php echo $item['brief_name'] ?></h1>
-            <span class="entermember">加入会员</span>
-            <div class="memberdescription">
-            	<?php 
-            		if (!empty($item['slogan'])) 
-            		echo ''.$item['slogan'].'';
-            		else{
-            			echo '会员享特权,多重优惠抢不停!';
-            		} 
-            	?>
-            </div>
-        </div>
-	</a>
-    <?php endforeach ?>
-    	</div>
-    	<div class="swiper-slide">
-    		<a class="entermember_wrap" href="https://www.517ybang.com/member_biz/detail?biz_id=142" target="_self">
-        <div class="member_list wid710 auto border20" style="background-color:#0081c9;background: url(https://jinlaisandbox-images.b0.upaiyun.com/ornament_biz/member_thumb_url/201801/0119/150239.png) no-repeat center center;">
-        	            <div class="memberlistpic" style="background-color:#0081c9">
-                <img src="https://jinlaisandbox-images.b0.upaiyun.com/ornament_biz/member_logo_url/201801/0119/150233.png">
-            </div>
-            <h1>游乐游儿童游泳</h1>
-            <span class="entermember">加入会员</span>
-            <div class="memberdescription">
-            	会员享特权,多重优惠抢不停!            </div>
-        </div>
-	</a>
-    	</div>
-    	<div class="swiper-slide">
-    	<a class="entermember_wrap" href="https://www.517ybang.com/member_biz/detail?biz_id=138" target="_self">
-        <div class="member_list wid710 auto border20" style="background-color:#bccdb6;background: url(https://jinlaisandbox-images.b0.upaiyun.com/ornament_biz/member_thumb_url/201801/0119/142820.png) no-repeat center center;">
-        	            <div class="memberlistpic" style="background-color:#bccdb6">
-                <img src="https://jinlaisandbox-images.b0.upaiyun.com/ornament_biz/member_logo_url/201801/0119/142515.png">
-            </div>
-            <h1>萃匠坚果</h1>
-            <span class="entermember">加入会员</span>
-            <div class="memberdescription">
-            	会员享特权,多重优惠抢不停!            </div>
-        </div>
-	</a>
-    	</div>
-    	<div class="swiper-slide">
-    	<a class="entermember_wrap" href="https://www.517ybang.com/member_biz/detail?biz_id=138" target="_self">
-        <div class="member_list wid710 auto border20" style="background-color:#bccdb6;background: url(https://jinlaisandbox-images.b0.upaiyun.com/ornament_biz/member_thumb_url/201801/0119/142820.png) no-repeat center center;">
-        	            <div class="memberlistpic" style="background-color:#bccdb6">
-                <img src="https://jinlaisandbox-images.b0.upaiyun.com/ornament_biz/member_logo_url/201801/0119/142515.png">
-            </div>
-            <h1>萃匠坚果</h1>
-            <span class="entermember">加入会员</span>
-            <div class="memberdescription">
-            	会员享特权,多重优惠抢不停!            </div>
-        </div>
-	</a>
-	<a class="entermember_wrap" href="https://www.517ybang.com/member_biz/detail?biz_id=142" target="_self">
-        <div class="member_list wid710 auto border20" style="background-color:#0081c9;background: url(https://jinlaisandbox-images.b0.upaiyun.com/ornament_biz/member_thumb_url/201801/0119/150239.png) no-repeat center center;">
-        	            <div class="memberlistpic" style="background-color:#0081c9">
-                <img src="https://jinlaisandbox-images.b0.upaiyun.com/ornament_biz/member_logo_url/201801/0119/150233.png">
-            </div>
-            <h1>游乐游儿童游泳</h1>
-            <span class="entermember">加入会员</span>
-            <div class="memberdescription">
-            	会员享特权,多重优惠抢不停!            </div>
-        </div>
-	</a>
-    	</div>
-    				<div class="swiper-paginationcoupon"></div>
-				</div>
-			</div>
-		
-		</div>
-   </div>
+input.goodsCheck:checked{
+	background:url(<?php echo CDN_URL ?>media/cart/icon/icon_radio4.png) no-repeat center left;
+	background-size:contain
+	}*/
+.shop-info-text h4 {
+	width: 3.92rem;
+    overflow: hidden;
+    line-height: .3rem;
+    height: .58rem;
+
+}
+.item{
+	padding-bottom: 2.5rem;
+}
+</style>
 
 <?php
     // 应用中不显示底部导航栏
@@ -188,14 +57,14 @@
 				</a>
 				<a href="https://www.517ybang.com/member_biz" target="_self">
 				<div class="itemmenulist">
-					<i class="homeHuiyuan homeHuiyuan1"></i>
-					<span class="text" style="color: #ff753e;">会员</span>
+					<i class="homeHuiyuan"></i>
+					<span class="text">会员</span>
 				</div>
 				</a>
 				<a href="https://www.517ybang.com/cart" target="_self">
 				<div class="itemmenulist">
-					<i class="homeGouwuche"></i>
-					<span class="text">购物车</span>
+					<i class="homeGouwuche homeGouwuche1"></i>
+					<span class="text"  style="color: #ff753e;">购物车</span>
 				</div>
 				</a>
 				<a href="https://www.517ybang.com/mine" target="_self">
@@ -206,121 +75,183 @@
 				</a>
 			</div>
 		</div>
-		</div>
-
+    </div>
 <?php endif ?>
-<script>
-			$(function(){
-				
-//					//会员卡顶部商家分类
-			$.ajax({
-				type:"post",
-				url:"https://api.517ybang.com/item_category/index",
-				data : {app_type:'client'},
-				dataType : 'json',
-				success:function(res){
-					for (var i = 0;i < res.content.length;i++) {
-						if(res.content[i].level == 1){
-							console.log(res.content[i].name);
-						var memberTitleList = '<div class="swiper-slide"><a>'+res.content[i].name+'</a></div>';
-						$('#memberTitle').append(memberTitleList);
-						}
-					}
-					var mySwipersuper = new Swiper('.swiper-containercouponcenter', {
-				        scrollbar: '.swiper-scrollbar123',
-				        scrollbarHide: false,
-				        slidesPerView: 'auto',
-				        centeredSlides: false,
-				        grabCursor: true,
-				        spaceBetween : 0,
-				        observer:true,
-				        observeParents:true
-				    });
-				    var swiperIndex = new Swiper('.swiper-containercouponcentertemplate', {
-			            pagination: '.swiper-paginationcoupon',
-			            paginationClickable: true,
-			//          autoplay: 2000,
-			//          loop:true
-						 onSlideChangeEnd: function (swiper) {
-						                var j=swiperIndex.activeIndex;
-						                $(".swiper-containercouponcenter a").removeClass("couponcenter");
-										$(".swiper-containercouponcenter a").eq(j).addClass('couponcenter');
-						            }
-					});
-					 $(".swiper-containercouponcenter a").on('click',function(){
-						var index = $(this).parents().index();
-						console.log(index);
-						$(".swiper-containercouponcenter a").removeClass("couponcenter");
-							$(this).addClass('couponcenter');
-//						 $(".paymentdetailsnav span").removeClass('paymentcur').eq(index).addClass('paymentcur');
-			            swiperIndex.slideTo(index,1000,false);
-						
-			});
-			var mo=function(e){e.preventDefault();};
 
-		/***禁止滑动***/
-		function stop(){
-				
-		        document.body.style.overflow='hidden';        
-		        document.addEventListener("touchmove",mo,false);//禁止页面滑动
+    <div class="item">
+        <div class="payment-bar">
+            <div class="all-checkbox fl"><input type="checkbox" class="check goods-check" id="AllCheck">
+                <span class="allchecktext">全选<span>
+            </div>
+            <div class="shop-total fl">
+                <strong>合计：<span>¥</span><i class="total" id="AllTotal">0.00</i></strong>
+                <p>不含运费</p>
+            </div>
+            <a href="#" class="settlement fr">结算</a>
+        </div>
+    </div>
+
+<script src="<?php echo CDN_URL ?>js/jqm.js"></script>
+<script>
+	$(function(){
+	    var params = common_params;
+	    var user_id = <?php echo $this->session->user_id ?>;
+	    var newShopInfo;
+	    //请求下购物车最新的商品信息,尤其是重复的商品数量
+	    $.ajax({
+	    	type:"post",
+	    	url:"https://api.517ybang.com/cart/sync_down",
+	    	dataType : 'json',
+	    	data :{app_type:'client',id:user_id},
+	    	async : false,
+	    	success : function(res){
+	    		newShopInfo = res.content.cart_string.split(',');
+	    	}
+	    });
+	 
+		$.ajax({
+		url: api_url + 'cart/index',
+		type: 'post',
+		dataType: 'json',
+		data : params,
+	success : function(res){
+		for (var i = 0;i<res.content.order_items.length;i++) {
+			var biz_name = res.content.order_items[i].biz_name;
+			var biz_img = "https://medias.517ybang.com/biz/" + res.content.order_items[i].biz_url_logo;
+			var list = '<div class="my-address-list wid710 auto border20 mt20 bgfff"><div class="shopping"><div class="shop-group-item"><div class="touch"><div class="buycarshoptitle"><div class="shop-name clearfix"><input type="checkbox" name="itemShopall" class="check goods-check shopCheck"><h4><img src="'+biz_img+'" /></h4><h3 class="fl">'+biz_name+'<i class="icon-Arrow"></i></h3></div></div></div></div></div></div>';
+			$('.item').append(list);
+			for (var j = 0;j < res.content.order_items[i].order_items.length;j++ ) {
+			    var order_item = res.content.order_items[i].order_items[j];
+			    //console.log(order_item);
+
+				var name = order_item.name;
+				var price = order_item.price;
+				var unit_name = order_item.unit_name;
+                // 生成订单所含商品主图URL
+                var item_image_url = (order_item.item_image.indexOf('http') === -1)? media_url+'item/'+order_item.item_image: order_item.item_image;
+				var item_id = res.content.order_items[i].order_items[j].item_id;
+				  for (var k = 0;k < newShopInfo.length;k++) {
+				   	//取出每一组数据,如果有多个商品,更新视图
+				   	if(item_id == newShopInfo[k].split('|')[1]){
+				   		var itemList = '<div class="itemlist"><div class="goodslist clearfix"><div class="shop-info clearfix"><input type="checkbox" name="item" class="check goods-check goodsCheck"><div class="shop-info-img"><a href="https://www.517ybang.com/item/detail?id='+item_id+'" target="_self"><img src="'+item_image_url+'" /></a></div><div class="shop-info-text fl"><a href="https://www.517ybang.com/item/detail?id='+item_id+'" target="_self"><h4>'+name+'</h4></a><div class="shop-price"><div class="shop-pices">￥<b class="price">'+price+'</b>/'+unit_name+'</div><div class="shop-arithmetic"><a href="javascript:;" class="minus fl"><i class="icon-jian" style="font-size: .44rem;"></i></a><span class="num fl" >'+newShopInfo[k].split('|')[3]+'</span><a href="javascript:;" class="plus active fl"><i class="icon-add-add-red" style="font-size: .44rem;"></i></a></div></div></div></div><div class="shopPrice">本店总计：￥<span class="shop-total-amount ShopTotal">0.00</span></div></div><a href="javascript:;" class="remove">删除</a></div>';
+				   	}
+				   		
+				   }
+				$('.touch').eq(i).append(itemList);
+			}
 		}
-		
-		/***取消滑动限制***/
-		function move(){
-		        document.body.style.overflow='';//出现滚动条
-		        document.removeEventListener("touchmove",mo,false);        
+		$(".minus").click(function() {
+		var t = $(this).parent().find(".num");
+		t.text(parseInt(t.text()) - 1);
+		if (t.text() <= 1) {
+			t.text(1);
+			$(this).children("i").removeClass("icon-jian-active")
 		}
-				$("#popwindowbtn").on("click",function(){
-				$(".popwindowcontent").animate({  
-				    left: 0,  
-				    opacity: 1  
-				}, 300, 'linear');  
-				stop();
-			});
-			$("#popwindowcancle").on("click",function(){
-				$(".popwindowcontent").animate({  
-				    left: '7.5rem',  
-				    opacity: 0  
-				}, 300, 'linear');  
-				move();
-			
-			});
-			$(".popwindowcontent input").focus(function(){
-				$(this).css({
-					'text-align':'left',
-					'text-indent':'.8rem'
-				});
-				$(this).next('i').animate({  
-				    left: '.6rem',  
-				}, 100, 'linear');  
-			}).blur(function(){
-				$(this).css({
-					'text-align':'center',
-					'text-indent':'0'
-				});
-				$(this).next('i').animate({  
-				    left: '2.3rem',  
-				}, 100, 'linear');  
-			});
-			$('#search-membercard').keydown(function(event){
-				//event.preventDefault();
-				if(event.which == 13){
-					alert('查询会员卡信息~');
+		TotalPrice()
+	});
+	$(".plus").click(function() {
+		var t = $(this).parent().find(".num");
+		t.text(parseInt(t.text()) + 1);
+		if (t.text() <= 1) {
+			t.text(1)
+		}
+		$(this).siblings(".minus").children("i").addClass("icon-jian-active");
+		TotalPrice()
+	});
+	
+	var checkFlag = 0;
+	$(".goodsCheck").click(function(event) {
+		var goods = $(this).closest(".shop-group-item").find(".goodsCheck");
+		var goodsC = $(this).closest(".shop-group-item").find(".goodsCheck:checked");
+		var Shops = $(this).closest(".shop-group-item").find(".shopCheck");
+		if (goods.length == goodsC.length) {
+			Shops.prop("checked", true);
+			if ($(".shopCheck").length == $(".shopCheck:checked").length) {
+				$("#AllCheck").prop("checked", true);
+				TotalPrice()
+			} else {
+				$("#AllCheck").prop("checked", false);
+				TotalPrice()
+			}
+		} else {
+			Shops.prop("checked", false);
+			$("#AllCheck").prop("checked", false);
+			TotalPrice()
+		}
+	});
+	$(".shopCheck").click(function() {
+		if ($(this).prop("checked") == true) {
+			$(this).parents(".shop-group-item").find(".goods-check").prop("checked", true);
+			if ($(".shopCheck").length == $(".shopCheck:checked").length) {
+				$("#AllCheck").prop("checked", true);
+				TotalPrice()
+			} else {
+				$("#AllCheck").prop("checked", false);
+				TotalPrice()
+			}
+		} else {
+			$(this).parents(".shop-group-item").find(".goods-check").prop("checked", false);
+			$("#AllCheck").prop("checked", false);
+			TotalPrice()
+		}
+	});
+	$("#AllCheck").click(function() {
+		if ($(this).prop("checked") == true) {
+			$(".goods-check").prop("checked", true);
+			TotalPrice()
+		} else {
+			$(".goods-check").prop("checked", false);
+			TotalPrice()
+		}
+		$(".shopCheck").change()
+	});
+
+	function TotalPrice() {
+		var allprice = 0;
+		$(".shop-group-item").each(function() {
+			var oprice = 0;
+			$(this).find(".goodsCheck").each(function() {
+				if ($(this).is(":checked")) {
+					var num = parseInt($(this).parents(".shop-info").find(".num").text());
+					var price = parseFloat($(this).parents(".shop-info").find(".price").text());
+					var total = price * num;
+					oprice += total
 				}
-			})
-			
-			//如果不存在会员卡头像,那么显示默认头像
-			$('.memberlistpic').find('img').each(function(i){
-				if($(this).attr('src') == 'https://medias.517ybang.com/biz/'){
-					$(this).attr('src','https://cdn-remote.517ybang.com/media/home/default.png');
-				}
+				$(this).closest(".shop-group-item").find(".ShopTotal").text(oprice.toFixed(2))
 			});
-					
-				}
-			});
-		
-			
+			var oneprice = parseFloat($(this).find(".ShopTotal").text());
+			allprice += oneprice
 		});
-		
-			
+		$("#AllTotal").text(allprice.toFixed(2))
+	}
+	$(".itemlist").on("swipeleft", function() {
+		$(".itemlist").addClass("unselected");
+		$(".itemlist").removeClass("selected");
+		$(this).addClass("selected");
+		$(this).removeClass("unselected");
+		$(this).find("a.remove").on("click", function() {
+			var touchId = $(this).parents(".itemlist");
+			if (touchId.siblings().length - 1 == 0) {
+				touchId.parents(".touch").parents(".shop-group-item").remove();
+				TotalPrice()
+			}
+			TotalPrice();
+			touchId.css("border", "0");
+			touchId.stop().animate({
+				height: "0",
+				margin: "0"
+			}, 300, function() {
+				$(this).remove();
+				TotalPrice()
+			})
+		})
+	}).on("swiperight", function() {
+		$(this).parents(".touch").find(".itemlist").addClass("unselected")
+	});
+	}
+
+})
+	})
+	
 </script>
+
