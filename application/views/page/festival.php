82adf6df2169d03b539203d4612d6caa77548e19
roseQing0627 <rosese_qing0623@163.com>
2018-11-05 14:53:38 +0800

    xqq

---
+++ application/views/page/festival.php
@@ -0,0 +1,178 @@
+
+    <link rel="stylesheet" href="<?php echo CDN_URL ?>css/normal.css"/>
+    <link rel="stylesheet" href="<?php echo CDN_URL ?>css/base.css"/>
+    <script src="<?php echo CDN_URL ?>js/rem.js"></script>
+    <style>
+        html{ margin: 0 auto; height: 100%}
+        body {width: 100%;background-color: #F2F2F3; height: 100%}
+        a {text-decoration: none;}
+        .box {width: 100%; height: 31rem; position: relative;background: url("<?php echo CDN_URL ?>media/imageBG.jpg") no-repeat;background-size: 100% 100%;
+
+        }
+        .item-list{
+            margin: 0 auto;
+            width: 4.8rem;
+            padding-top: 11.58rem;
+            font-size: 0.26rem;
+        }
+        .festival-li{
+            width: 100%;
+            height: 2.3rem;
+            display: flex;
+
+        }
+        .festival-left{
+            display: block;
+            width: 2.25rem;
+            height: 2.25rem;
+        }
+        .festival-left img{
+            display: block;
+            width: 2.25rem;
+            height: 2.25rem;
+        }
+        .festival-right {
+            flex: 1;
+            padding: 0 0.1rem;
+        }
+        .item-name{
+            font-size: 0.28rem;
+            color: #ffffff;
+            padding: 0.2rem 0 0.08rem 0;
+            width: 2.5rem;
+            white-space:nowrap;overflow:hidden;text-overflow:ellipsis;
+        }
+        .item-slogan{
+            font-size: 0.24rem;
+            color: #ffffff;
+            line-height: 0.26rem;
+            width: 2.5rem;
+            white-space:nowrap;overflow:hidden;text-overflow:ellipsis;
+        }
+        .item-price{
+            font-size: 0.3rem;
+            color: #ff5061;
+            line-height:0.62rem;
+            width: 2.5rem;
+            margin-bottom:0.03rem;
+            white-space:nowrap;overflow:hidden;text-overflow:ellipsis;
+
+        }
+        .go-pay{
+            font-size: 0.24rem;
+            color: #ffffff;
+            background-color: #ff5061;
+            padding: 0.08rem 0.4rem;
+            border-radius: 0.2rem;
+        }
+        .chat-img{
+            width:1.3rem;
+            height:1.3rem;
+            display:block;
+            position: absolute;
+            right:0.22rem;
+            top:6.9rem;
+        }
+    </style>
+
+
+<!--href="https://www.517ybang.com/item/detail?id=4037"-->
+<div class="box">
+    <img class="chat-img" src="<?php echo CDN_URL ?>media/chatImg.png" alt=""/>
+    <ul class="item-list">
+        <li class="festival-li" style="margin-bottom: 0.4rem;">
+            <a class="festival-left" href="<?php echo base_url('item/detail?id=6664&fromavt=yes') ?>">
+                <img src="<?php echo CDN_URL ?>media/festival1.png" alt=""/>
+            </a>
+            <div class="festival-right">
+                <a class="item-text" href="<?php echo base_url('item/detail?id=6664&fromavt=yes') ?>">
+                    <p class="item-name">东坡灶250g*4</p>
+                    <p class="item-slogan">老式五仁青红丝月饼</p>
+                    <p class="item-price"><del style="color:#ccc">¥168.00</del>¥68.00/盒</p>
+                    <a href="<?php echo base_url('item/detail?id=6664&fromavt=yes') ?>" class="go-pay" data-id="6664" >立即购买</a>
+                 </a>
+            </div>
+        </li>
+        <li class="festival-li" style="margin-bottom: 0.55rem;">
+            <a class="festival-left" href="<?php echo base_url('item/detail?id=5858&fromavt=yes') ?>">
+                <img src="<?php echo CDN_URL ?>media/festival2.png" alt=""/>
+            </a>
+            <div class="festival-right">
+                <a class="item-text" href="<?php echo base_url('item/detail?id=5858&fromavt=yes') ?>">
+                    <p class="item-name">大荔冬枣</p>
+                    <p class="item-slogan">500g/份</p>
+                    <p class="item-price"><del style="color:#ccc">¥58.00</del>¥9.90</p>
+                    <a href="<?php echo base_url('item/detail?id=5858&fromavt=yes') ?>" class="go-pay" data-id="5883" >立即购买</a>
+                </a>
+            </div>
+        </li>
+        <li class="festival-li" style="margin-bottom: 0.48rem;">
+            <a class="festival-left" href="<?php echo base_url('item/detail?id=6655&fromavt=yes') ?>">
+                <img src="<?php echo CDN_URL ?>media/festival3.png" alt=""/>
+            </a>
+            <div class="festival-right">
+                <a class="item-text" href="<?php echo base_url('item/detail?id=6655&fromavt=yes') ?>">
+                    <p class="item-name">龙大压榨一级</p>
+                    <p class="item-slogan">花生油5L</p>
+                    <p class="item-price"><del style="color:#ccc">¥169.00</del>¥125.00</p>
+                    <a href="<?php echo base_url('item/detail?id=6655&fromavt=yes') ?>" class="go-pay" data-id="6176" >立即购买</a>
+                </a>
+            </div>
+        </li>
+        <li class="festival-li" style="margin-bottom: 0.48rem;">
+            <a class="festival-left" href="<?php echo base_url('item/detail?id=6605&fromavt=yes') ?>">
+                <img src="<?php echo CDN_URL ?>media/festival4.png" alt=""/>
+            </a>
+            <div class="festival-right">
+                <a class="item-text" href="<?php echo base_url('item/detail?id=6605&fromavt=yes') ?>">
+                    <p class="item-name">光明畅优酸奶</p>
+                    <p class="item-slogan">280g</p>
+                    <p class="item-price">¥8.90</p>
+                    <a href="<?php echo base_url('item/detail?id=6605&fromavt=yes') ?>" class="go-pay" data-id="6605" >立即购买</a>
+                </a>
+            </div>
+        </li>
+        <li class="festival-li" style="margin-bottom: 0.48rem;">
+            <a class="festival-left" href="<?php echo base_url('item/detail?id=6727&fromavt=yes') ?>">
+                <img src="<?php echo CDN_URL ?>media/festival5.png" alt=""/>
+            </a>
+            <div class="festival-right">
+                <a class="item-text" href="<?php echo base_url('item/detail?id=6727&fromavt=yes') ?>">
+                    <p class="item-name">小猪佩奇电动爬</p>
+                    <p class="item-slogan">楼梯1个</p>
+                    <p class="item-price">¥29.00</p>
+                    <a href="<?php echo base_url('item/detail?id=6727&fromavt=yes') ?>" class="go-pay" data-id="4904" >立即购买</a>
+                </a>
+            </div>
+        </li>
+        <li class="festival-li">
+            <a class="festival-left" href="<?php echo base_url('item/detail?id=4904&fromavt=yes') ?>">
+                <img src="<?php echo CDN_URL ?>media/festival6.png" alt=""/>
+            </a>
+            <div class="festival-right">
+                <a class="item-text" href="<?php echo base_url('item/detail?id=4904&fromavt=yes') ?>">
+                    <p class="item-name">沃隆混合坚果</p>
+                    <p class="item-slogan">700g/盒</p>
+                    <p class="item-price"><del style="color:#ccc">¥179.00</del>¥139.00</p>
+                    <a href="<?php echo base_url('item/detail?id=4904&fromavt=yes') ?>" class="go-pay" data-id="6716" >立即购买</a>
+                </a>
+            </div>
+        </li>
+    </ul>
+</div>
+<script>
+    $(function(){
+        /*$('.go-pay').on('click',function(){
+            var userId = user_id;
+        	if(userId == ''){
+        		window.location.href=base_url+'login';
+        		return false;
+        	}
+            var id = $(this).attr('data-id');
+            var target_url = base_url + 'order/create?immediately=yes&cart_string=0|'+id+'|0|1';
+            location.href = target_url;
+
+        });*/
+
+    })
+</script>
